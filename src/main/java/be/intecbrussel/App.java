22929533464f3f1d75f875a59c3809d413ee3225
michaelarayabe <michaelseifuaraya@gmail.com>
2020-10-08 15:25:12 +0200

    working with entitymanagerfactory and entitymanager and some small changes

--- src/main/java/be/intecbrussel/App.java
+++ src/main/java/be/intecbrussel/App.java
@@ -8,10 +8,30 @@ public class App {
 
     public static void main(String[] args) {
 
+        Alien a = new Alien();
+        a.setId(9);
+        a.setName("Maria");
+        a.setTech("Hardware");
+
+        Alien b = new Alien();
+        b.setId(122);
+        b.setTech("testing");
+
+        Alien c = new Alien();
+
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
         EntityManager em = emf.createEntityManager();
 
-        Alien a = em.find(Alien.class, 4);
+        c = em.find(Alien.class,2);
+        System.out.println(c);
+
+        em.getTransaction().begin();
+        em.persist(a);
+        em.persist(b);
+        em.getTransaction().commit();
+
         System.out.println(a);
+
     }
 }
